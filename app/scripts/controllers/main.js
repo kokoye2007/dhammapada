a2b904a4192663362a8da1215dc0b6f5023c5de1
Erwin Jansen <pokowaka@gmail.com>
2014-09-03 17:10:28 -0700

    Yeoman framework.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dhammapadaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dhammapadaApp
+ */
+angular.module('dhammapadaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
